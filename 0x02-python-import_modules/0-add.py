1cb586283b48ed3fae5e5ce216e25bf00213ee2a
kholoudelbatal <kholouud.elbatal@gmail.com>
2023-11-30 05:41:32 -0800

    0-add

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    from add_0 improt add:
+    from add_0 import add
     a = 1
     b = 2
-    result = a + b
     print("{} + {} = {}".format(a, b, add(a, b)))
