a3dc47889327c13797b36dba5bb3cefe37a3db37
kholoudelbatal <kholouud.elbatal@gmail.com>
2023-12-06 05:27:35 -0800

    1-search_replace.py

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-def uniq_add(my_list=[]):
-    return sum(set(my_list))
+def search_replace(my_list, search, replace):
+    return list(map(lambda e: replace if e == search else e, my_list))
